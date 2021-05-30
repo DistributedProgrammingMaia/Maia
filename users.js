1aa6dee48ef24653b94d140963ddc5f9ef59e442
Ebaneo <ebaneo_89@hotmail.com>
2021-05-30 17:32:24 +0200

    #4.api post users

--- users.js
+++ users.js
@@ -27,5 +27,33 @@ app.get('/api/patient/:id', function (req, res) {
     if (!patient) return res.status(404).send('the course not found');
     res.send(patient);
 })
+
+// HTTP POST
+
+app.post('/api/patient', (req, res) => {
+    const { error } = validateUser(req.body) // ==result.error
+    if (error) {
+        //400 bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const patient = {
+        id: patientsList.lenght + 1,
+        name: req.body.firstName,
+        lastName: req.body.lastName,
+        birthDay: req.body.birthDay,
+        gender: req.body.gender,
+        fileName: req.body.fileName,
+        diagnosis: req.body.diagnosis,
+        email: req.body.email,
+        type: req.body.type
+
+
+
+    };
+    patientsList.push(patient);
+    res.send(patientsList);
+});
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
